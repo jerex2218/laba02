523fe623222d90774ff66327bdea9fe6e282618e
jerex2218 <ktotonekto98@gmail.com>
2025-04-10 20:26:22 +0000

    Apply clang-format Mozilla style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#include <string>   
-int main() { // начало
-    std::string name; //переменная
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello world from " << name << endl;
-    return 0; //конец
+#include <string>
+int
+main()
+{                   // начало
+  std::string name; // переменная
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  std::cout << "Hello world from " << name << endl;
+  return 0; // начало
 }
