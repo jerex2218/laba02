053b69387df787cc9b65dd5b8f096cd31a5d09f9
jerex2218 <tprivet92@gmail.com>
2025-04-10 20:50:20 +0000

    translate comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>   
-int main() { // start
-    std::string name; //variable
+int main() { // начало
+    std::string name; //переменная
     std::cout << "Enter your name: ";
     std::cin >> name;
     std::cout << "Hello world from " << name << endl;
-    return 0; //end
+    return 0; //конец
 }
